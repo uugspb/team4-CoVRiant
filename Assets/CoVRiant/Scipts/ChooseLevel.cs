38679567d277a530848447f56b0157f74f7a47bf
georgy <joragod@yandex.ru>
2016-04-03 12:04:50 +0300

    [+] jumping

--- Assets/CoVRiant/Scipts/ChooseLevel.cs
+++ Assets/CoVRiant/Scipts/ChooseLevel.cs
@@ -15,8 +15,10 @@ public class ChooseLevel : MonoBehaviour {
 
     public Material onHighlightMaterial;
 
+    
     // Use this for initialization
     void Start () {
+
         lcamera = GameObject.FindWithTag("lcamera");
         levels = new GameObject[countOfLevels];
         distnces = new Vector3[countOfLevels];
@@ -42,10 +44,19 @@ public class ChooseLevel : MonoBehaviour {
             {
                 minAngle = nAngle;
                 num = i;
+
             }
         }
         if (prevLevel != num)
         {
+            if (num != -1)
+            {
+                iTween.MoveBy(levels[num], iTween.Hash("y", levels[num].transform.localScale.y / 6, "time", 0.6F, "looptype", "none", "easetype", "easeInOutExpo"));
+                iTween.RotateBy(levels[num], iTween.Hash("x", 1.0F, "time", 0.3F, "easetype", "easeInOutExpo", "looptype", "none"));
+                iTween.ScaleTo(levels[num], iTween.Hash("scale", levels[num].transform.localScale * 1.3F, "time", 0.3F, "easetype", "easeInOutExpo", "looptype", "none"));
+                iTween.ScaleTo(levels[num], iTween.Hash("scale", levels[num].transform.localScale, "time", 0.3F, "delay", 0.3F, "easetype", "easeInOutExpo", "looptype", "none"));
+                iTween.MoveTo(levels[num], iTween.Hash("position", levels[num].transform.position, "time", 0.3F, "delay", 0.6F, "looptype", "none", "easetype", "easeInOutExpo"));
+            }
             if (num != -1) levels[num].GetComponent<Renderer>().material = onHighlightMaterial;
             if (prevLevel == 0) levels[0].GetComponent<Renderer>().material = defaultMaterial1;
             if (prevLevel == 1) levels[1].GetComponent<Renderer>().material = defaultMaterial2;
